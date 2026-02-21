c70469e7e9ce792f77c11c55a64c97070baa3e7a
Jan Rádl <radl.honza@gmail.com>
2026-02-21 22:34:31 +0100

    New texts up to 22

--- screens/HexagramDetailScreen.tsx
+++ screens/HexagramDetailScreen.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import { View, ScrollView } from "react-native";
 import { Hexagram } from "../data/hexagrams";
-import { HexagramDetailTab } from "../types/generic";
+import { HexagramDetailTab, TLanguage } from "../types/generic";
 import TopNavigationBarHexagramDetail from "../components/navbars/TopNavigationBarHexagramDetail";
 import BottomNavigationBarHexagramDetail from "../components/navbars/BottomNavigationBarHexagramDetail";
 import GeistMonoText from "../components/typography/GeistMonoText";
-import BodoniText from "../components/typography/BodoniText";
 import i18n from "../i18n/config";
+import { getYinYangTranslation } from "../data/yinyang";
 
 interface HexagramDetailScreenProps {
   hexagram: Hexagram;
@@ -27,7 +27,7 @@ export default function HexagramDetailScreen({
         return (
           <View className="p-6">
             <GeistMonoText className="text-text text-base leading-7">
-              {hexagram.content[i18n.language as "cs" | "en"].meaning}
+              {hexagram.content[i18n.language as TLanguage].meaning}
             </GeistMonoText>
           </View>
         );
@@ -35,23 +35,54 @@ export default function HexagramDetailScreen({
         return (
           <View className="p-6">
             <GeistMonoText className="text-text text-base leading-7">
-              {hexagram.content[i18n.language as "cs" | "en"].oracle}
+              {hexagram.content[i18n.language as TLanguage].oracle}
             </GeistMonoText>
           </View>
         );
       case "anatomy":
         return (
           <View className="p-6">
-            <GeistMonoText className="text-text text-base leading-7">
-              todo
-            </GeistMonoText>
+            <View className="divide-y divide-dashed divide-text/25">
+              {hexagram.content[i18n.language as "cs" | "en"].anatomy.map(
+                (item) => {
+                  return (
+                    <View
+                      key={item.type + "-" + item.name}
+                      className="py-4 flex gap-1"
+                    >
+                      <View className="w-10 rounded bg-main p-0.5 mb-1">
+                        <GeistMonoText
+                          variant="medium"
+                          className="text-text text-xs text-center"
+                        >
+                          {getYinYangTranslation(
+                            item.type,
+                            i18n.language as TLanguage,
+                          )}
+                        </GeistMonoText>
+                      </View>
+                      <GeistMonoText
+                        className="text-text text-base"
+                        variant="bold"
+                      >
+                        {item.position}. linie - {item.name}
+                      </GeistMonoText>
+
+                      <GeistMonoText className="text-text text-base leading-7">
+                        {item.description}{" "}
+                      </GeistMonoText>
+                    </View>
+                  );
+                },
+              )}
+            </View>
           </View>
         );
       case "evolution":
         return (
           <View className="p-6">
             <GeistMonoText className="text-text text-base leading-7">
-              {hexagram.content[i18n.language as "cs" | "en"].evolution}
+              {hexagram.content[i18n.language as TLanguage].evolution}
             </GeistMonoText>
           </View>
         );
@@ -73,20 +104,17 @@ export default function HexagramDetailScreen({
           {hexagram.romanization}
         </GeistMonoText>
         <GeistMonoText variant="bold" className="text-3xl text-text pt-1 -mt-1">
-          {hexagram.content[i18n.language as "cs" | "en"].name}
+          {hexagram.content[i18n.language as TLanguage].name}
         </GeistMonoText>
       </View>
       <ScrollView
         className="flex-1 z-1 bg-primary"
         contentContainerStyle={{ paddingTop: 0, paddingBottom: 100 }}
       >
-        <View className="bg-primary absolute w-full h-full z-0"/>
+        <View className="bg-primary absolute w-full h-full z-0" />
 
         {renderTabContent()}
       </ScrollView>
-      
-      
-
 
       <BottomNavigationBarHexagramDetail
         activeTab={activeTab}
